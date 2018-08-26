a79e309860ef5a4b9bfe73544996aac0a60d8788
aoisupersix <aoisupersix@gmail.com>
2018-08-26 09:28:27 +0900

    [add] ステータスの詳細情報を追加

--- public/js/index.js
+++ public/js/index.js
@@ -56,19 +56,21 @@ function init(rootSnap) {
   statusSnap = status;
   for(var i = 0; i < members.length; i++){
     var stateId = parseInt(members[i]["status"]);
+    var lastStateId = parseInt(members[i]["last_status"]);
     addMemberRow(
       i,
-      members[i]["last_name"] + "　" + members[i]["first_name"],
+      members[i]["last_name"] + " " + members[i]["first_name"],
       status[stateId]["name"],
       status[stateId]["color"],
-      members[i]["last_update_date"]
+      members[i]["last_update_date"],
+      status[lastStateId]["name"],
+      members[i]["last_update_is_auto"]
     );
   }
 }
 
 /**
  * メンバー情報の更新を行います。
- * AndroidApp側から呼び出される関数です。
  * @param {Number} memberId メンバーのID
  * @param {DataSnapShot} memberSnap メンバー情報
  */
@@ -76,6 +78,7 @@ function updateMemberStatus(memberId, memberSnap) {
   $('#memberStatus').children().each(function(index, element) {
     var card = $(element).find('.btn');
     var stateId = parseInt(memberSnap["status"]);
+    var lastStateId = parseInt(memberSnap["last_status"]);
     if($(card).attr('data-id') == memberId) {
       //ステータスの更新
       var color = $(card).attr('data-color');
@@ -83,7 +86,10 @@ function updateMemberStatus(memberId, memberSnap) {
         'data-id': memberId,
         'data-name': memberSnap["last_name"] + "　" + memberSnap["first_name"],
         'data-statusText': statusSnap[stateId]["name"],
-        'data-color': statusSnap[stateId]["color"]
+        'data-color': statusSnap[stateId]["color"],
+        'data-lastUpdateDate': memberSnap["last_update_date"],
+        'data-lastStatusText': statusSnap[lastStateId]["name"],
+        'data-lastUpdateIsAuto': memberSnap["last_update_is_auto"]
       });
       $(element).removeClass('table-' + color);
       $(element).addClass('table-' + statusSnap[stateId]["color"]);
@@ -100,10 +106,15 @@ function updateMemberStatus(memberId, memberSnap) {
  */
 function showStatusDetail(obj) {
   console.log(obj);
+  //Bind
   updateWeeklyGauge($(obj).attr('data-id'));
   $('#statusDetail-Name').text($(obj).attr('data-name'));
   $('#statusDetail-Status').text($(obj).attr('data-statusText'));
+  $('#statusDetail-lastUpdateDate').text($(obj).attr('data-lastUpdateDate'));
+  $('#statusDetail-lastStatusText').text($(obj).attr('data-lastStatusText'));
+  $('#statusDetail-statusText').text($(obj).attr('data-statusText'));
   $('#statusDetailModal').attr('data-id', $(obj).attr('data-id'));
+  $(obj).attr('data-lastUpdateIsAuto') == "true" ? $("#statusDetail-lastUpdateIsAuto").text("自動更新") : $("#statusDetail-lastUpdateIsAuto").text("手動更新")
   $('#statusDetailModal').modal();
 }
 
@@ -114,8 +125,10 @@ function showStatusDetail(obj) {
  * @param {string} statusText - ステータス状態を表す文字列
  * @param {string} color - ステータス状態に対応するBootStrapカラー
  * @param {string} lastUpdateDate - 最終更新時間
+ * @param {string} lastStatusText - 最終更新ステータス状態を表す文字列
+ * @param {boolean} lastUpdateIsAuto - 最終更新が自動更新か？ 
  */
-function addMemberRow(id, name, statusText, color, lastUpdateDate){
+function addMemberRow(id, name, statusText, color, lastUpdateDate, lastStatusText, lastUpdateIsAuto){
   $('#memberStatus').append(
     $('<tr></tr>').addClass('table-' + color)
     .append($('<th></th>')
@@ -125,7 +138,10 @@ function addMemberRow(id, name, statusText, color, lastUpdateDate){
           'data-id': id,
           'data-name': name,
           'data-statusText': statusText,
-          'data-color': color
+          'data-color': color,
+          'data-lastUpdateDate': lastUpdateDate,
+          'data-lastStatusText': lastStatusText,
+          'data-lastUpdateIsAuto': lastUpdateIsAuto
         })
       )
     )
@@ -140,17 +156,17 @@ function addMemberRow(id, name, statusText, color, lastUpdateDate){
  */
 function initWeeklyGauge() {
   var gaugeConf = liquidFillGaugeDefaultSettings();
-  gaugeConf.circleColor = "#D4AB6A";
+  gaugeConf.circleColor = "#FF871B";
   gaugeConf.textColor = "#553300";
   gaugeConf.waveTextColor = "#805615";
-  gaugeConf.waveColor = "#AA7D39";
+  gaugeConf.waveColor = "#78C7E7";
   gaugeConf.circleThickness = 0.1;
   gaugeConf.circleFillGap = 0.2;
   gaugeConf.textVertPosition = 0.8;
   gaugeConf.waveAnimateTime = 2000;
   gaugeConf.waveHeight = 0.3;
   gaugeConf.waveCount = 1;
-  weeklyGauge = loadLiquidFillGauge("weeklyGauge", 60.1, gaugeConf);
+  weeklyGauge = loadLiquidFillGauge("weeklyGauge", 0, gaugeConf);
 }
 
 /**
